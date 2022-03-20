00d4de2578cf6618de9e0162680d1403679f6fb2
Ashish Patel <toaashishpatel@gmail.com>
2022-03-20 22:34:30 +0530

    Issue #1 fixed:  Cluster creation fails

--- src/eks-cluster.tf
+++ src/eks-cluster.tf
@@ -5,7 +5,7 @@ resource "aws_eks_cluster" "this" {
   version  = "1.21"
 
   vpc_config {
-    security_group_ids      = [aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id]
+    # security_group_ids      = [aws_security_group.eks_cluster.id, aws_security_group.eks_nodes.id] # already applied to subnet
     subnet_ids              = flatten([aws_subnet.public[*].id, aws_subnet.private[*].id])
     endpoint_private_access = true
     endpoint_public_access  = true
